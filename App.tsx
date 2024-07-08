a8d19bf76788ef72f0df01aae9aa1ea8c888f716
Borges10002 <borges10002@gmail.com>
2024-07-07 20:11:14 -0400

    update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+import { OneSignal } from "react-native-onesignal";
+
+OneSignal.initialize("01125e6e-716e-447f-b569-478ef4b61909");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
--- README.md
+++ README.md
@@ -7,7 +7,6 @@
   <a href="LICENSE"><img  src="https://img.shields.io/static/v1?label=License&message=MIT&color=8257e5&labelColor=202024" alt="License"></a>
 </p>
 
-
 ## 💻 Projeto
 
 igniteshoesapp
@@ -22,7 +21,6 @@ Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para ma
   Feito com 💜 by Rocketseat
 </p>
 
-
 <!--START_SECTION:footer-->
 
 <br />
@@ -35,4 +33,3 @@ Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para ma
 </p>
 
 <!--END_SECTION:footer-->
-
