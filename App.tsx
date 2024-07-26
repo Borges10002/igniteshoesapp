9f6ab9559df0c741bf86f7f31dc75d7b3893a960
Borges10002 <borges10002@gmail.com>
2024-07-25 21:58:33 -0400

    update

--- App.tsx
+++ App.tsx
@@ -14,12 +14,15 @@ import { Loading } from "./src/components/Loading";
 import { CartContextProvider } from "./src/contexts/CartContext";
 
 import { OneSignal } from "react-native-onesignal";
+import { tagUserEmailCreate } from "./src/notifications/notificationsTags";
 
 OneSignal.initialize("01125e6e-716e-447f-b569-478ef4b61909");
 OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("borges10002@gmail.com");
 
   return (
     <NativeBaseProvider theme={THEME}>
---
+++ src/notifications/notificationsTags.ts
@@ -0,0 +1,5 @@
+import { OneSignal } from "react-native-onesignal";
+
+export function tagUserEmailCreate(email: string) {
+  OneSignal.User.addTag("user_email", email);
+}
