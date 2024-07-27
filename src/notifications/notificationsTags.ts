280752dcaec3d4096ba9fc5f41601f326048cfc0
Borges10002 <borges10002@gmail.com>
2024-07-26 20:49:59 -0400

    update
    ;

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -3,3 +3,7 @@ import { OneSignal } from "react-native-onesignal";
 export function tagUserEmailCreate(email: string) {
   OneSignal.User.addTag("user_email", email);
 }
+
+export function tagUserEmailRemove(email: string) {
+  OneSignal.User.removeTag("user_email");
+}
