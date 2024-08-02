d74b3fbc9b7ccc6094422085857445a3c7e5f993
Borges10002 <borges10002@gmail.com>
2024-08-01 21:50:43 -0400

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,37 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from "./app.routes";
+import { useEffect } from "react";
+import {
+  NotificationWillDisplayEvent,
+  OneSignal,
+} from "react-native-onesignal";
 
 export function Routes() {
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  useEffect(() => {
+    const handleNotification = (event: NotificationWillDisplayEvent): void => {
+      event.preventDefault();
+      const response = event.getNotification();
+    };
+
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      handleNotification
+    );
+
+    return () => {
+      OneSignal.Notifications.removeEventListener(
+        "foregroundWillDisplay",
+        handleNotification
+      );
+    };
+  }, []);
 
   return (
     <NavigationContainer theme={theme}>
